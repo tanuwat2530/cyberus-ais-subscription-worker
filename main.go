19f84b74a419e4cc6efeb88dd4573020931532b0
Tanuwat Chaisin <tanuwat_cha@truedigital.com>
2025-04-09 17:50:02 +0700

    Consumer run forever

--- main.go
+++ main.go
@@ -17,9 +17,9 @@ var count = int64(100)                       // Limit to 100 keys per scan
 func main() {
 	// Start background worker in a goroutine
 	go backgroundWorker()
-
 	// Keep main running so the program doesn't exit
 	select {} // This blocks forever
+
 }
 
 // This is the background job that runs forever
@@ -33,16 +33,13 @@ func backgroundWorker() {
 		PoolSize: 10,               //Connection pools
 	})
 
-	for t := range ticker.C {
-		fmt.Println("Background job running at:", t.Format("15:04:05"))
-		// Initialize Redis client
-
+	for range ticker.C {
+		// Perform the scan with the match pattern and count
+		keys, newCursor, err := rdb.Scan(ctx, cursor, matchPattern, count).Result()
+		if err != nil {
+			panic(err)
+		}
 		for {
-			// Perform the scan with the match pattern and count
-			keys, newCursor, err := rdb.Scan(ctx, cursor, matchPattern, count).Result()
-			if err != nil {
-				panic(err)
-			}
 			// Process only the first key found (if any)
 			if len(keys) >= LIMIT_KEY {
 				for i := 0; i < LIMIT_KEY; i++ {
@@ -54,6 +51,7 @@ func backgroundWorker() {
 						fmt.Println("Error getting value:", err)
 					} else {
 						fmt.Println("Value:", val)
+
 					}
 
 				}
