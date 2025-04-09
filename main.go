2b3cc5f2b06f0e3f81c9811b29b0fdc0053c92c5
Tanuwat Chaisin <tanuwat_cha@truedigital.com>
2025-04-10 00:48:42 +0700

    Set Redis after send request

--- main.go
+++ main.go
@@ -6,30 +6,32 @@ import (
 	"sync"
 	"time"
 
+	"encoding/json"
+
 	"github.com/redis/go-redis/v9"
 )
-
-var ctx = context.Background()
-var LIMIT_KEY = 10
-var cursor uint64 = 0
-var matchPattern = "subscription-callback:*" // Pattern to match keys
-var count = int64(100)                       // Limit to 100 keys per scan
 
 func main() {
 	// Start background worker in a goroutine
 	go backgroundWorker()
 	// Keep main running so the program doesn't exit
 	select {} // This blocks forever
-
 }
 
 // This is the background job that runs forever
 func backgroundWorker() {
-	fmt.Println("##### START WORKER PROCESS #####")
+
+	var ctx = context.Background()
+	var LIMIT_KEY = 10
+	var cursor uint64 = 0
+	var matchPattern = "subscription-callback:*" // Pattern to match keys
+	var count = int64(100)                       // Limit to 100 keys per scan
+
+	fmt.Println("##### BACKGROUUND PROCESS RUNNING #####")
 	ticker := time.NewTicker(1 * time.Second)
 	defer ticker.Stop()
 	rdb := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379", // Change if needed
+		Addr:     "127.0.0.1:6379", // Change if needed
 		Password: "",               // No password by default
 		DB:       0,                // Default DB
 		PoolSize: 10,               //Connection pools
@@ -38,14 +40,14 @@ func backgroundWorker() {
 	var wg sync.WaitGroup
 
 	for range ticker.C {
+
 		// Perform the scan with the match pattern and count
 		keys, newCursor, err := rdb.Scan(ctx, cursor, matchPattern, count).Result()
-
 		if err != nil {
 			panic(err)
 		}
-		for { // for infinity loop
 
+		for { // for infinity loop
 			// Process only the first key found (if any)
 			if len(keys) >= LIMIT_KEY {
 				for i := 0; i < LIMIT_KEY; i++ {
@@ -58,7 +60,7 @@ func backgroundWorker() {
 						//fmt.Println("Value:", val)
 						// Start multiple goroutines (threads)
 						wg.Add(1)
-						go worker(i, &wg, valJson)
+						go threadWorker(i, &wg, valJson)
 						rdb.Del(ctx, keys[i]).Result()
 						// Wait for all goroutines to finish
 					}
@@ -75,31 +77,70 @@ func backgroundWorker() {
 					//fmt.Println("Value:", val)
 					// Start multiple goroutines (threads)
 					wg.Add(1)
-					go worker(0, &wg, valJson)
+					go threadWorker(0, &wg, valJson)
 					rdb.Del(ctx, keys[0]).Result()
 					// Wait for all goroutines to finish
 				}
-
 			}
 
 			// Update cursor for the next iteration
 			cursor = newCursor
-
 			// If the cursor is 0, then the scan is complete
 			if cursor == 0 {
 				break
 			}
-
 		}
-
 	}
 }
 
 // Function that simulates work for a thread
-func worker(id int, wg *sync.WaitGroup, data string) {
+func threadWorker(id int, wg *sync.WaitGroup, jsonString string) {
+	type DataRequest struct {
+		ClientIP    string `json:"ClientIP"`
+		ProviderUrl string `json:"ProviderUrl"`
+		Action      string `json:"action"`
+		Code        string `json:"code"`
+		Desc        string `json:"desc"`
+		Media       string `json:"media"`
+		Msisdn      string `json:"msisdn"`
+		Operator    string `json:"operator"`
+		Refid       string `json:"refid"`
+		Shortcode   string `json:"shortcode"`
+		Timestamp   string `json:"timestamp"`
+		Token       string `json:"token"`
+		Tranref     string `json:"tranref"`
+		ClientID    string `json:"ClientID"`
+		RedisKey    string `Json:"RedisKey"`
+	}
+
+	var ctx = context.Background()
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "127.0.0.1:6379", // Change if needed
+		Password: "",               // No password by default
+		DB:       0,                // Default DB
+		PoolSize: 10,               //Connection pools
+	})
+
 	defer wg.Done() // Mark this goroutine as done when it exits
 	fmt.Printf("Worker No : %d\n", id)
-	fmt.Println("Process data : " + data)
-	time.Sleep(10000 * time.Millisecond) // Simulate work
-	fmt.Printf("Worker No : %d finished\n", id)
+
+	// Convert struct to JSON string
+	var dataRequest DataRequest
+	err := json.Unmarshal([]byte(jsonString), &dataRequest)
+	if err != nil {
+		fmt.Println("JSON Marshal error:", err)
+	}
+	fmt.Println("sending request : " + dataRequest.RedisKey)
+
+	redis_key := "send-request:" + dataRequest.RedisKey
+	ttl := 240 * time.Hour // expires in 10 day
+
+	// Set key with TTL
+	err = rdb.Set(ctx, redis_key, "return", ttl).Err()
+	if err != nil {
+		fmt.Println("Redis SET error:", err)
+
+	}
+
+	fmt.Printf("Worker No : %d SET REDIS finished\n", id)
 }
