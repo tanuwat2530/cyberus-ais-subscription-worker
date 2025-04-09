7df1cf9e8fa908f6707acaafec9002033eb9f6ac
Tanuwat Chaisin <tanuwat_cha@truedigital.com>
2025-04-09 19:16:51 +0700

    Update consumer with multi threading process

--- main.go
+++ main.go
@@ -3,13 +3,14 @@ package main
 import (
 	"context"
 	"fmt"
+	"sync"
 	"time"
 
 	"github.com/redis/go-redis/v9"
 )
 
 var ctx = context.Background()
-var LIMIT_KEY = 5
+var LIMIT_KEY = 10
 var cursor uint64 = 0
 var matchPattern = "subscription-callback:*" // Pattern to match keys
 var count = int64(100)                       // Limit to 100 keys per scan
@@ -24,6 +25,7 @@ func main() {
 
 // This is the background job that runs forever
 func backgroundWorker() {
+	fmt.Println("##### START WORKER PROCESS #####")
 	ticker := time.NewTicker(1 * time.Second)
 	defer ticker.Stop()
 	rdb := redis.NewClient(&redis.Options{
@@ -33,27 +35,49 @@ func backgroundWorker() {
 		PoolSize: 10,               //Connection pools
 	})
 
+	var wg sync.WaitGroup
+
 	for range ticker.C {
 		// Perform the scan with the match pattern and count
 		keys, newCursor, err := rdb.Scan(ctx, cursor, matchPattern, count).Result()
+
 		if err != nil {
 			panic(err)
 		}
-		for {
+		for { // for infinity loop
+
 			// Process only the first key found (if any)
 			if len(keys) >= LIMIT_KEY {
 				for i := 0; i < LIMIT_KEY; i++ {
-					fmt.Println("Send Key to Worker : ", keys[i]) // Print only the first key
-
+					//fmt.Println("Send Key to Worker : ", keys[i]) // Print only the first key
 					// Example: Get the value of the key (assuming it's a string)
-					val, err := rdb.Get(ctx, keys[i]).Result()
+					valJson, err := rdb.Get(ctx, keys[i]).Result()
 					if err != nil {
 						fmt.Println("Error getting value:", err)
 					} else {
-						fmt.Println("Value:", val)
-
+						//fmt.Println("Value:", val)
+						// Start multiple goroutines (threads)
+						wg.Add(1)
+						go worker(i, &wg, valJson)
+						rdb.Del(ctx, keys[i]).Result()
+						// Wait for all goroutines to finish
 					}
+				}
+			}
 
+			if len(keys) > 0 && len(keys) < LIMIT_KEY {
+				//fmt.Println("Send Key to Worker : ", keys[i]) // Print only the first key
+				// Example: Get the value of the key (assuming it's a string)
+				valJson, err := rdb.Get(ctx, keys[0]).Result()
+				if err != nil {
+					fmt.Println("Error getting value:", err)
+				} else {
+					//fmt.Println("Value:", val)
+					// Start multiple goroutines (threads)
+					wg.Add(1)
+					go worker(0, &wg, valJson)
+					rdb.Del(ctx, keys[0]).Result()
+					// Wait for all goroutines to finish
 				}
 
 			}
@@ -69,4 +93,13 @@ func backgroundWorker() {
 		}
 
 	}
+}
+
+// Function that simulates work for a thread
+func worker(id int, wg *sync.WaitGroup, data string) {
+	defer wg.Done() // Mark this goroutine as done when it exits
+	fmt.Printf("Worker No : %d\n", id)
+	fmt.Println("Process data : " + data)
+	time.Sleep(10000 * time.Millisecond) // Simulate work
+	fmt.Printf("Worker No : %d finished\n", id)
 }
